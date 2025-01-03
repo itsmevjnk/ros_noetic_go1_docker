90d63eca6bae78b06fffd47ca5c000a46014baf2
Thanh Vinh Nguyen <ngtv0404@gmail.com>
2025-01-03 12:07:49 +1100

    parameterise UDP IP addresses

--- src/unitree_ros_io/src/driver_node.cpp
+++ src/unitree_ros_io/src/driver_node.cpp
@@ -26,12 +26,14 @@ public:
     LowState low_state = {0};
 
 public:
-    Custom()
+    Custom(const char* low_udp_addr, const char* high_udp_addr)
         : 
         // low_udp(LOWLEVEL),
-        low_udp(LOWLEVEL, 8091, "192.168.123.10", 8007),
-        high_udp(8090, "192.168.12.1", 8082, sizeof(HighCmd), sizeof(HighState)) // 192.168.123.161 if running on robot, 192.168.12.1 otherwise
+        low_udp(LOWLEVEL, 8091, low_udp_addr, 8007),
+        high_udp(8090, high_udp_addr, 8082, sizeof(HighCmd), sizeof(HighState)) // 192.168.123.161 if running on robot, 192.168.12.1 otherwise
     {
+        ROS_INFO("low_udp: %s, high_udp: %s", low_udp_addr, high_udp_addr);
+
         high_udp.InitCmdData(high_cmd);
         low_udp.InitCmdData(low_cmd);
 
@@ -70,7 +72,7 @@ public:
     }
 };
 
-Custom custom;
+Custom* custom = nullptr;
 
 ros::Subscriber sub_cmd_vel;
 ros::Publisher pub_high;
@@ -79,15 +81,20 @@ long cmd_vel_count = 0;
 
 void cmdVelCallback(const geometry_msgs::Twist::ConstPtr &msg)
 {
+    if (!custom)
+    {
+        ROS_ERROR("custom object is null!");
+        return;
+    }
     // printf("cmdVelCallback is running!\t%ld\n", cmd_vel_count);
 
-    custom.high_cmd = rosMsg2Cmd(msg);
+    custom->high_cmd = rosMsg2Cmd(msg);
 
-    ROS_INFO("/cmd_vel received: vx=%f, vy=%f, rot=%f", custom.high_cmd.velocity[0], custom.high_cmd.velocity[1], custom.high_cmd.yawSpeed);
+    ROS_INFO("/cmd_vel received: vx=%f, vy=%f, rot=%f", custom->high_cmd.velocity[0], custom->high_cmd.velocity[1], custom->high_cmd.yawSpeed);
 
-    // printf("cmd_x_vel = %f\n", custom.high_cmd.velocity[0]);
-    // printf("cmd_y_vel = %f\n", custom.high_cmd.velocity[1]);
-    // printf("cmd_yaw_vel = %f\n", custom.high_cmd.yawSpeed);
+    // printf("cmd_x_vel = %f\n", custom->high_cmd.velocity[0]);
+    // printf("cmd_y_vel = %f\n", custom->high_cmd.velocity[1]);
+    // printf("cmd_yaw_vel = %f\n", custom->high_cmd.yawSpeed);
 
     // printf("cmdVelCallback ending!\t%ld\n\n", cmd_vel_count++);
 }
@@ -97,11 +104,16 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "twist_sub");
 
     ros::NodeHandle nh;
+    
+    std::string low_addr, high_addr;
+    nh.param<std::string>("low_addr", low_addr, "192.168.123.10");
+    nh.param<std::string>("high_addr", high_addr, "192.168.12.1");
+    custom = new Custom(low_addr.c_str(), high_addr.c_str());
 
     sub_cmd_vel = nh.subscribe("cmd_vel", 1, cmdVelCallback);
 
-    LoopFunc loop_udpSend("high_udp_send", 0.002, 3, boost::bind(&Custom::highUdpSend, &custom));
-    LoopFunc loop_udpRecv("high_udp_recv", 0.002, 3, boost::bind(&Custom::highUdpRecv, &custom));
+    LoopFunc loop_udpSend("high_udp_send", 0.002, 3, boost::bind(&Custom::highUdpSend, custom));
+    LoopFunc loop_udpRecv("high_udp_recv", 0.002, 3, boost::bind(&Custom::highUdpRecv, custom));
 
     loop_udpSend.start();
     loop_udpRecv.start();
@@ -123,16 +135,16 @@ int main(int argc, char **argv)
         msg_imu.header.seq = count;
         msg_imu.header.stamp = current_time;
         msg_imu.header.frame_id = "odom";
-        msg_imu.orientation.w = custom.high_state.imu.quaternion[0];
-        msg_imu.orientation.x = custom.high_state.imu.quaternion[1];
-        msg_imu.orientation.y = custom.high_state.imu.quaternion[2];
-        msg_imu.orientation.z = custom.high_state.imu.quaternion[3];
-        msg_imu.angular_velocity.x = custom.high_state.imu.gyroscope[0];
-        msg_imu.angular_velocity.y = custom.high_state.imu.gyroscope[1];
-        msg_imu.angular_velocity.z = custom.high_state.imu.gyroscope[2];
-        msg_imu.linear_acceleration.x = custom.high_state.imu.accelerometer[0];
-        msg_imu.linear_acceleration.y = custom.high_state.imu.accelerometer[1];
-        msg_imu.linear_acceleration.z = custom.high_state.imu.accelerometer[2];
+        msg_imu.orientation.w = custom->high_state.imu.quaternion[0];
+        msg_imu.orientation.x = custom->high_state.imu.quaternion[1];
+        msg_imu.orientation.y = custom->high_state.imu.quaternion[2];
+        msg_imu.orientation.z = custom->high_state.imu.quaternion[3];
+        msg_imu.angular_velocity.x = custom->high_state.imu.gyroscope[0];
+        msg_imu.angular_velocity.y = custom->high_state.imu.gyroscope[1];
+        msg_imu.angular_velocity.z = custom->high_state.imu.gyroscope[2];
+        msg_imu.linear_acceleration.x = custom->high_state.imu.accelerometer[0];
+        msg_imu.linear_acceleration.y = custom->high_state.imu.accelerometer[1];
+        msg_imu.linear_acceleration.z = custom->high_state.imu.accelerometer[2];
         // ROS_INFO("publishing /imu");
         pub_imu.publish(msg_imu);
 
@@ -141,16 +153,16 @@ int main(int argc, char **argv)
         msg_odom.header.stamp = current_time;
         msg_odom.header.frame_id = "odom";
         msg_odom.child_frame_id = "base_link";
-        msg_odom.pose.pose.position.x = custom.high_state.position[0];
-        msg_odom.pose.pose.position.y = custom.high_state.position[1];
-        msg_odom.pose.pose.position.z = custom.high_state.position[2];
-        msg_odom.pose.pose.orientation.w = custom.high_state.imu.quaternion[0];
-        msg_odom.pose.pose.orientation.x = custom.high_state.imu.quaternion[1];
-        msg_odom.pose.pose.orientation.y = custom.high_state.imu.quaternion[2];
-        msg_odom.pose.pose.orientation.z = custom.high_state.imu.quaternion[3];
-        msg_odom.twist.twist.linear.x = custom.high_state.velocity[0];
-        msg_odom.twist.twist.linear.y = custom.high_state.velocity[1];
-        msg_odom.twist.twist.angular.z = custom.high_state.velocity[2];
+        msg_odom.pose.pose.position.x = custom->high_state.position[0];
+        msg_odom.pose.pose.position.y = custom->high_state.position[1];
+        msg_odom.pose.pose.position.z = custom->high_state.position[2];
+        msg_odom.pose.pose.orientation.w = custom->high_state.imu.quaternion[0];
+        msg_odom.pose.pose.orientation.x = custom->high_state.imu.quaternion[1];
+        msg_odom.pose.pose.orientation.y = custom->high_state.imu.quaternion[2];
+        msg_odom.pose.pose.orientation.z = custom->high_state.imu.quaternion[3];
+        msg_odom.twist.twist.linear.x = custom->high_state.velocity[0];
+        msg_odom.twist.twist.linear.y = custom->high_state.velocity[1];
+        msg_odom.twist.twist.angular.z = custom->high_state.velocity[2];
         // ROS_INFO("publishing /odom");
         pub_odom.publish(msg_odom);
         
@@ -158,13 +170,13 @@ int main(int argc, char **argv)
 		odom_trans.header.stamp = current_time;
 		odom_trans.header.frame_id = "odom";
 		odom_trans.child_frame_id = "base_link";
-		odom_trans.transform.translation.x = custom.high_state.position[0];
-		odom_trans.transform.translation.y = custom.high_state.position[1];
-		odom_trans.transform.translation.z = custom.high_state.position[2];
-		odom_trans.transform.rotation.w = custom.high_state.imu.quaternion[0];
-		odom_trans.transform.rotation.x = custom.high_state.imu.quaternion[1];
-		odom_trans.transform.rotation.y = custom.high_state.imu.quaternion[2];
-		odom_trans.transform.rotation.z = custom.high_state.imu.quaternion[3];
+		odom_trans.transform.translation.x = custom->high_state.position[0];
+		odom_trans.transform.translation.y = custom->high_state.position[1];
+		odom_trans.transform.translation.z = custom->high_state.position[2];
+		odom_trans.transform.rotation.w = custom->high_state.imu.quaternion[0];
+		odom_trans.transform.rotation.x = custom->high_state.imu.quaternion[1];
+		odom_trans.transform.rotation.y = custom->high_state.imu.quaternion[2];
+		odom_trans.transform.rotation.z = custom->high_state.imu.quaternion[3];
         // ROS_INFO("publishing odom -> base_link transform");
 		odom_broadcaster.sendTransform(odom_trans);
 
