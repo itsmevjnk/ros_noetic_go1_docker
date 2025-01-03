c8532ef63e6c9505b85fd4506b5e5940b4c19e5a
Thanh Vinh Nguyen <ngtv0404@gmail.com>
2025-01-03 11:39:14 +1100

    add /odom publishing and fix IP address

--- src/unitree_ros_io/src/driver_node.cpp
+++ src/unitree_ros_io/src/driver_node.cpp
@@ -30,7 +30,7 @@ public:
         : 
         // low_udp(LOWLEVEL),
         low_udp(LOWLEVEL, 8091, "192.168.123.10", 8007),
-        high_udp(8090, "192.168.123.161", 8082, sizeof(HighCmd), sizeof(HighState))
+        high_udp(8090, "192.168.12.1", 8082, sizeof(HighCmd), sizeof(HighState)) // 192.168.123.161 if running on robot, 192.168.12.1 otherwise
     {
         high_udp.InitCmdData(high_cmd);
         low_udp.InitCmdData(low_cmd);
@@ -57,7 +57,6 @@ public:
 
     void lowUdpRecv()
     {
-
         low_udp.Recv();
         low_udp.GetRecv(low_state);
     }
@@ -134,7 +133,26 @@ int main(int argc, char **argv)
         msg_imu.linear_acceleration.x = custom.high_state.imu.accelerometer[0];
         msg_imu.linear_acceleration.y = custom.high_state.imu.accelerometer[1];
         msg_imu.linear_acceleration.z = custom.high_state.imu.accelerometer[2];
+        ROS_INFO("publishing /imu");
         pub_imu.publish(msg_imu);
+
+        nav_msgs::Odometry msg_odom;
+        msg_odom.header.seq = count;
+        msg_odom.header.stamp = current_time;
+        msg_odom.header.frame_id = "odom";
+        msg_odom.child_frame_id = "base_link";
+        msg_odom.pose.pose.position.x = custom.high_state.position[0];
+        msg_odom.pose.pose.position.y = custom.high_state.position[1];
+        msg_odom.pose.pose.position.z = custom.high_state.position[2];
+        msg_odom.pose.pose.orientation.w = custom.high_state.imu.quaternion[0];
+        msg_odom.pose.pose.orientation.x = custom.high_state.imu.quaternion[1];
+        msg_odom.pose.pose.orientation.y = custom.high_state.imu.quaternion[2];
+        msg_odom.pose.pose.orientation.z = custom.high_state.imu.quaternion[3];
+        msg_odom.twist.twist.linear.x = custom.high_state.velocity[0];
+        msg_odom.twist.twist.linear.y = custom.high_state.velocity[1];
+        msg_odom.twist.twist.angular.z = custom.high_state.velocity[2];
+        ROS_INFO("publishing /odom");
+        pub_odom.publish(msg_odom);
         
 		geometry_msgs::TransformStamped odom_trans;
 		odom_trans.header.stamp = current_time;
@@ -147,6 +165,7 @@ int main(int argc, char **argv)
 		odom_trans.transform.rotation.x = custom.high_state.imu.quaternion[1];
 		odom_trans.transform.rotation.y = custom.high_state.imu.quaternion[2];
 		odom_trans.transform.rotation.z = custom.high_state.imu.quaternion[3];
+        ROS_INFO("publishing odom -> base_link transform");
 		odom_broadcaster.sendTransform(odom_trans);
 
         ros::spinOnce(); loop_rate.sleep();
